4ea5beb639a831a6511db28e57b5f86cec9255c8
Mirkiri <103731038+Mirkiri@users.noreply.github.com>
2022-06-26 16:42:50 +0800

    Update FoodComponentMixin.java

--- src/main/java/net/mirkiri/alwayseat/mixin/FoodComponentMixin.java
+++ src/main/java/net/mirkiri/alwayseat/mixin/FoodComponentMixin.java
@@ -1,14 +1,9 @@
 package net.mirkiri.alwayseat.mixin;
 
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.util.registry.Registry;
-import net.mirkiri.alwayseat.config.AlwaysEatConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.HashMap;
 
